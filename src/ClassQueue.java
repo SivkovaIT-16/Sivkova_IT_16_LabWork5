f1b25658e7666c66e4b0b5ed3c90e579e36efff7
Ekaterina Sivkova <Katasivkova352@gmail.com>
2025-11-11 23:04:55 +0500

    Initial Commit

--- src/ClassQueue.java
+++ src/ClassQueue.java
@@ -3,34 +3,34 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ClassQueue {
-    private Queue<Number> input;
+    private Queue<Number> queue;
     private  boolean result;
 
-    public Queue<Number> getInput() {
-        return input;
+    public Queue<Number> getQueue() {
+        return queue;
     }
 
     public boolean getResult() {
         return result;
     }
 
-    public void setInput(Queue<Number> input) {
-        Validator.validateNullQueue(input);
-        this.input = input;
+    public void setQueue(Queue<Number> queue) {
+        Validator.validateNullQueue(queue);
+        this.queue = queue;
     }
 
     public ClassQueue(){
-        this.input = new LinkedList<>();
+        this.queue = new LinkedList<>();
     }
 
     public ClassQueue(Queue<Number> input) {
         Validator.validateNullQueue(input);
-        this.input = input;
+        this.queue = input;
     }
 
     @Override
     public String toString() {
-        return "Очередь: " + input +"\n" +
+        return "Очередь: " + queue +"\n" +
                 "Результат проверки: " + result;
     }
 
@@ -38,20 +38,21 @@ public class ClassQueue {
         if (i < 0 || j < 0 || i >= j) {
             throw new IllegalArgumentException("Некорректные индексы: i должно быть меньше j и оба >= 0");
         }
-        if (input.size() <= j) {
+        if (queue.size() <= j) {
             throw new IllegalArgumentException("Индекс j превышает размер очереди");
         }
-        Iterator<Number> iterator = input.iterator();
+        Iterator<Number> iterator = queue.iterator();
         for (int k = 0; k < i; k++) {
             iterator.next();
         }
         Number first = iterator.next();
+        result = true;
         for (int k = i + 1; k <= j; k++) {
             Number current = iterator.next();
             if(!first.equals(current)) {
                 result = false;
+                break;
             }
         }
-        result = true;
     }
 }
